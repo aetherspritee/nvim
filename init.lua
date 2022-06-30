44d71b340f2ee6bd5fe946940f2d440e3d9a8167
aetherspritee <schautendustin@gmail.com>
2022-06-30 19:27:41 +0200

    yo

---
+++ init.lua
@@ -0,0 +1,26 @@
+require("user.options") -- basic options
+require("user.keymaps") -- basic keymaps
+require("user.plugins") -- plugin management
+require("user.impatient") -- easy faster loading
+require("user.colorscheme") -- a e s t h e t i c s
+require("user.cmp") -- basic completion
+require("user.telescope") -- telescope
+require("user.telescope-file-browser") -- FIXME: doesnt work yet
+require("user.whichkey") -- see binds + add some more
+require("user.treesitter") -- treesitter
+require("user.autopairs") -- (((((())))))
+require("user.comment") -- FIXME: weird errors
+--require("user.gitsigns") -- git push
+require("user.nvim-tree") -- look at files mmmmh nice
+require("user.bufferline") -- no point but  a e s t h e t i c s
+require("user.lualine") -- command line thingy TODO: needs recoloring maybe
+require("user.toggleterm") -- fanceee term
+require("user.project") -- TODO: what does this do
+require("user.indentline") -- eyes need
+require("user.alpha") -- start page nice and  a e s t h e t i c
+require("user.bfs") -- no clue honestly
+require("user.lsp") -- lsp
+require("user.dap") -- hyper bug repellent
+-- TODO: autocommands maybe
+-- TODO: some more keybinds for moving text, buffer switching and split management
+-- TODO: org mode?
---
+++ testing.py
@@ -0,0 +1,10 @@
+def test(x):
+    print(x)
+    
+    if isinstance(x, str):
+        y = "this is a string as well!"
+    else:
+        y = 324
+
+
+test(4)
